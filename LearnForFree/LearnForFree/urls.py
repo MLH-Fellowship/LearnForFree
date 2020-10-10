798f26e6138257cd6f8b4e1bb7abf17863401a0c
Jordan C <nykezgaming@live.com>
2020-10-10 14:15:23 -0400

    update index

--- LearnForFree/LearnForFree/urls.py
+++ LearnForFree/LearnForFree/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -24,4 +26,4 @@ urlpatterns = [
     # ex: search/javascript/
     path('search/<str:keywords>/', views.results),
     path('admin/', admin.site.urls)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
