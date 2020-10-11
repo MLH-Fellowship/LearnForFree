1491f99d746586c0e0eb83a79fc0839bfd5972dd
Jordan C <nykezgaming@live.com>
2020-10-11 01:05:06 -0400

    Update settings.py

--- LearnForFree/LearnForFree/settings.py
+++ LearnForFree/LearnForFree/settings.py
@@ -117,9 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_DIRS = (
-     os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
